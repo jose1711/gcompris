2cb8a6750aff6b8b046fb0cd3c41348f15cea5d7
Johnny Jazeix <jazeix@gmail.com>
2016-11-11 00:06:03 +0100

    add logs

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -108,7 +108,7 @@ int main(int argc, char *argv[])
     // Disable it because we already support HDPI display natively
     qunsetenv("QT_DEVICE_PIXEL_RATIO");
 
-    QApplication::setAttribute(Qt::AA_UseOpenGLES);
+    QCoreApplication::setAttribute(Qt::AA_UseOpenGLES);
     QApplication app(argc, argv);
     app.setOrganizationName("KDE");
     app.setApplicationName(GCOMPRIS_APPLICATION_NAME);
